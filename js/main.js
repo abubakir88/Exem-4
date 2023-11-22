42c02ab015e5199f57f47dd71b602babf545b66c
abubakir88 <abubakirsaidxonov88@gmail.com>
2023-11-22 20:45:07 +0500

    kkk

--- js/main.js
+++ js/main.js
@@ -4,9 +4,16 @@ modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
 
-const header = document.querySelector("body");
+// const header = document.querySelector("body");
+// const menuBtn = document.querySelector(".menu");
+
+// menuBtn.addEventListener("click", () => {
+//   header.classList.toggle(".open");
+// });
+
+const header = document.querySelector("header");
 const menuBtn = document.querySelector(".menu");
 
 menuBtn.addEventListener("click", () => {
-  header.classList.toggle(".open");
+  header.classList.toggle("open");
 });
